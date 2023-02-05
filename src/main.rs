c7996a87178b58959413e72338bfc7e3579551de
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-02-05 14:09:20 -0300

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use bevy::{prelude::*, window::WindowResizeConstraints};
+use mst_visualizer::AppPlugins;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                title: "Graph visualizer".to_string(),
+                width: 800.0,
+                height: 600.0,
+                resizable: true,
+                resize_constraints: WindowResizeConstraints {
+                    min_width: 800.0,
+                    min_height: 600.0,
+                    ..default()
+                },
+                ..default()
+            },
+            ..default()
+        }))
+        .add_plugins(AppPlugins)
+        .run();
 }
