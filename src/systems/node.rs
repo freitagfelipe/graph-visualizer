9a67c07cb509141741fc732e5044fcb1e5f3bee0
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-02-05 11:47:51 -0300

    Add systems to control node movimentation

--- src/systems/node.rs
+++ src/systems/node.rs
@@ -1,4 +1,4 @@
-use crate::components::Node;
+use crate::components::{MovingNode, Node};
 use crate::resources::{NodeSettings, VisualizerState};
 use crate::utils;
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
@@ -81,6 +81,103 @@ pub fn remove_node(
             if let Some(entity_to_despawn) = entity_to_despawn {
                 commands.entity(entity_to_despawn).despawn();
             }
+        }
+    }
+}
+
+pub fn mark_node_to_move(
+    mut commands: Commands,
+    query: Query<(Entity, &Transform, &Handle<ColorMaterial>)>,
+    buttons: Res<Input<MouseButton>>,
+    windows: Res<Windows>,
+    node_settings: Res<NodeSettings>,
+    mut visualizer_state: ResMut<VisualizerState>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    if visualizer_state.is_moving_node {
+        return;
+    }
+
+    if buttons.pressed(MouseButton::Left) {
+        let window = windows
+            .get_primary()
+            .expect("Can not get the primary window");
+
+        if let Some((x, y)) = utils::get_mouse_coordinates(window) {
+            let mut node_to_mark = None;
+
+            for (entity, transform, color_material) in query.iter() {
+                if utils::is_mouse_on_node(
+                    x,
+                    y,
+                    transform.translation.x,
+                    transform.translation.y,
+                    node_settings.radius,
+                ) {
+                    node_to_mark = Some((entity, color_material));
+                }
+            }
+
+            if let Some((entity, color_material)) = node_to_mark {
+                commands.entity(entity).insert(MovingNode);
+                visualizer_state.is_moving_node = true;
+
+                if let Some(mut color_material) = materials.get_mut(color_material) {
+                    color_material.color = node_settings.selected_color;
+                }
+            }
+        }
+    }
+}
+
+pub fn move_node(
+    mut query: Query<(&mut Transform, With<MovingNode>)>,
+    buttons: Res<Input<MouseButton>>,
+    windows: Res<Windows>,
+    visualizer_state: Res<VisualizerState>,
+) {
+    if !visualizer_state.is_moving_node {
+        return;
+    }
+
+    if buttons.pressed(MouseButton::Left) {
+        let window = windows
+            .get_primary()
+            .expect("Can not get the primary window");
+
+        if let Some((x, y)) = utils::get_mouse_coordinates(window) {
+            for (mut transform, _) in query.iter_mut() {
+                transform.translation.x = x;
+                transform.translation.y = y;
+            }
+        }
+    }
+}
+
+pub fn unmark_node_that_was_moving(
+    mut commands: Commands,
+    query: Query<(Entity, &Handle<ColorMaterial>, With<MovingNode>)>,
+    buttons: Res<Input<MouseButton>>,
+    node_settings: Res<NodeSettings>,
+    mut visualizer_state: ResMut<VisualizerState>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    if !visualizer_state.is_moving_node {
+        return;
+    }
+
+    if buttons.just_released(MouseButton::Left) {
+        visualizer_state.is_moving_node = false;
+
+        let (entity, color_material, _) = query
+            .iter()
+            .next()
+            .expect("Can not get the node that was moving");
+
+        commands.entity(entity).remove::<MovingNode>();
+
+        if let Some(mut color_material) = materials.get_mut(color_material) {
+            color_material.color = node_settings.base_color;
         }
     }
 }
