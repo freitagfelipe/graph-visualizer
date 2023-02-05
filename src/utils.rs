ff5b6489fe908aac4476b409f5846ffc384dd97d
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-02-05 11:38:14 -0300

    Add an utility function to check if the mouse is above the node

--- src/utils.rs
+++ src/utils.rs
@@ -9,3 +9,10 @@ pub fn get_mouse_coordinates(window: &Window) -> Option<(f32, f32)> {
         mouse_position.y - window_center_y,
     ))
 }
+
+pub fn is_mouse_on_node(mouse_x: f32, mouse_y: f32, node_x: f32, node_y: f32, radius: f32) -> bool {
+    let distance_between_node_and_mouse =
+        ((mouse_x - node_x).powi(2) + (mouse_y - node_y).powi(2)).sqrt();
+
+    distance_between_node_and_mouse <= radius
+}
