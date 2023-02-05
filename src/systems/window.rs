5734030e35f0e82403484b69cf39c04bddf945c8
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-02-05 12:51:14 -0300

    Add a system to control window mode

---
+++ src/systems/window.rs
@@ -0,0 +1,15 @@
+use bevy::prelude::*;
+
+pub fn switch_screen_mode(input: Res<Input<KeyCode>>, mut windows: ResMut<Windows>) {
+    if input.just_released(KeyCode::F11) {
+        let window = windows
+            .get_primary_mut()
+            .expect("Can not get the primary window");
+
+        if window.mode() == WindowMode::Windowed {
+            window.set_mode(WindowMode::BorderlessFullscreen);
+        } else {
+            window.set_mode(WindowMode::Windowed);
+        }
+    }
+}
