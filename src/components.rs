627dd9abd95c4c14a20683a9b5a84cdab409924a
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-02-08 06:21:08 -0300

    Add a component for neighbor nodes

--- src/components.rs
+++ src/components.rs
@@ -8,3 +8,11 @@ pub struct MovingNode;
 
 #[derive(Component)]
 pub struct SelectedNode;
+
+#[derive(Component)]
+pub struct NeighborNodes {
+    pub v: Entity,
+    pub u: Entity,
+    pub pos_v: Transform,
+    pub pos_u: Transform,
+}
