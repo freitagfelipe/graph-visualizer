e68c373ed44c733fb412597c7c811c1e3358c8f4
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-02-09 02:43:19 -0300

    Add bloom settings in setup system

--- src/systems/setup.rs
+++ src/systems/setup.rs
@@ -1,5 +1,18 @@
-use bevy::prelude::*;
+use bevy::{core_pipeline::bloom::BloomSettings, prelude::*};
 
 pub fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        Camera2dBundle {
+            camera: Camera {
+                hdr: true,
+                ..default()
+            },
+            transform: Transform::from_xyz(0.0, 0.0, 1000.0 - 0.1),
+            ..default()
+        },
+        BloomSettings {
+            threshold: 0.9,
+            ..default()
+        },
+    ));
 }
