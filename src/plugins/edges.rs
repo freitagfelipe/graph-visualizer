c25729a47900056569b3207816677194b49fd7eb
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-02-09 19:32:52 -0300

    Update systems dependencies to avoid race conditions

--- src/plugins/edges.rs
+++ src/plugins/edges.rs
@@ -16,10 +16,14 @@ impl Plugin for EdgesPlugin {
             .add_event::<UpdateEdgeEvent>()
             .add_event::<RemoveEdgeEvent>()
             .add_event::<CreateOrUnspawnEdgeEvent>()
-            .add_system(edges::emit_create_or_unspawn_edge_event)
+            .add_system(
+                edges::emit_create_or_unspawn_edge_event.before(node::mark_node_to_create_edge),
+            )
             .add_system(edges::create_or_unspawn_edge)
             .add_system(
-                edges::update_edge_after_moving_node.after(node::fix_off_screen_node_positions),
+                edges::update_edge_after_moving_node
+                    .after(node::fix_off_screen_node_positions)
+                    .after(node::emit_update_edge_event_after_node_collision),
             )
             .add_system(edges::remove_edge_after_remove_node);
     }
--- src/plugins/node.rs
+++ src/plugins/node.rs
@@ -9,7 +9,7 @@ impl Plugin for NodePlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<NodeSettings>()
             .add_event::<ChangeNodeColorEvent>()
-            .add_system(node::spawn_node.before(node::unmark_node_that_was_moving))
+            .add_system(node::spawn_node)
             .add_system(node::remove_node)
             .add_system(node::mark_node_to_move)
             .add_system(node::move_node)
@@ -20,6 +20,7 @@ impl Plugin for NodePlugin {
                 node::fix_off_screen_node_positions
                     .after(node::spawn_node)
                     .after(node::move_node),
-            );
+            )
+            .add_system(node::emit_update_edge_event_after_node_collision);
     }
 }
